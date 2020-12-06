02dbb66ed52a38e53735e8169fbe5f79afe9985c
Xuankun <xzeng17@illinois.edu>
2020-12-06 15:49:36 -0600

    upload course project around

---
+++ elasticsearch_client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "context"
+    "fmt"
+
+    "github.com/olivere/elastic"
+)
+
+const (
+        ES_URL = "http://104.154.103.119:9200"
+)
+
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+    client, err := elastic.NewClient(
+        elastic.SetURL(ES_URL),
+        elastic.SetBasicAuth("root", "12345678"))
+    if err != nil {
+        return nil, err
+    }
+
+    searchResult, err := client.Search().
+        Index(index).
+        Query(query).
+        Pretty(true).
+        Do(context.Background())
+    if err != nil {
+        return nil, err
+    }
+
+    return searchResult, nil
+}
+
+func saveToES(i interface{}, index string, id string) error {
+    client, err := elastic.NewClient(
+        elastic.SetURL(ES_URL),
+        elastic.SetBasicAuth("root", "12345678"))
+    if err != nil {
+        return err
+    }
+
+    _, err = client.Index().
+        Index(index).
+        Id(id).
+        BodyJson(i).
+        Do(context.Background())
+
+    if err != nil {
+        return err
+    }
+    fmt.Printf("Post is saved to Elasticsearch: %s\n", id)
+    return nil
+}
